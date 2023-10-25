2182df1f0583dcd8a7812e394ad4292455892c4c
yoni de bleeker <yoni.de.bleeker@student.howest.be>
2023-10-25 09:33:40 +0200

    setup laravel + jwt auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Modules\Services\UserService;
+
+class AuthController extends Controller
+{
+    private $_service;
+    public function __construct(UserService $service)
+    {
+        $this->_service = $service;
+    }
+
+    public function register(Request $request)
+    {
+        $data = $request->only(['name', 'email', 'password']);
+        $result = $this->_service->registerUser($data);
+
+        return response()->noContent();
+    }
+
+    public function login(Request $request) {
+        $data = $request->only(['email', 'password']);
+        $token = $this->_service->login($data);
+
+        if (!$token) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+        return response([
+            "status" => "success",
+            "authorisation" => [
+                'token' => $token,
+                'type' => "bearer"
+            ]
+        ], 200);
+
+        /*
+        return response([
+            "status" => "success"
+        ], 200)->withCookie(
+            'token',
+            $token,
+            config('jwt.ttl'),
+            '/',
+            null,
+            true,
+            true,
+            false,
+            "None"
+        );*/
+    }
+
+}
